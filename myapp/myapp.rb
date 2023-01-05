cfd3240b889480e2d7de7043e8ecced2946446fd
Sanshiro Yarita <sanchanex@MacBook-Air-3.local>
2023-01-05 16:44:53 +0900

    [add] get tasks

--- myapp/myapp.rb
+++ myapp/myapp.rb
@@ -1,5 +1,16 @@
 require 'sinatra'
 
+tasks = [
+  {
+    title: 'フロントエンドの実装',
+    createdAt: Time.now
+  },
+  {
+    title: 'バックエンドの実装',
+    createdAt: Time.now
+  }
+]
+
 get '/' do
   'Hello world!'
 end
@@ -7,5 +18,11 @@ end
 get '/api/hello' do
   {
     message: 'Hello world!'
+  }.to_json
+end
+
+get '/api/tasks' do
+  {
+    tasks: tasks
   }.to_json
 end
